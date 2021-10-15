7c0d8d55d73d3c73e707d5a0b860505b63c55534
Sergei Kanter <sdkanter@outlook.com>
2021-10-15 14:39:34 +0200

    Добавьте файлы проекта.

---
+++ Ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            Console.WriteLine("Имя основного каталога: {0}", path);
+            Console.WriteLine("Входящие в него каталоги:");
+            foreach (DirectoryInfo dirName in dir.GetDirectories())
+            {
+                Console.WriteLine("Имя каталога: {0}",dirName.Name);
+                Console.WriteLine("Содержимое подкаталога {0}:", dirName.Name);
+                Console.WriteLine("Каталоги:");
+                foreach (DirectoryInfo subDirName in dirName.GetDirectories())
+                {
+                    Console.WriteLine(subDirName.Name);
+                }
+                Console.WriteLine("Файлы:");
+                foreach (FileInfo fileName in dirName.GetFiles())
+                {
+                    Console.WriteLine(fileName.Name);
+                }
+                Console.WriteLine("-----");
+            }
+            Console.ReadKey();
+        }
+    }
+}
