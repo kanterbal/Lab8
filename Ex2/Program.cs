2bb8f5b8d0269a27db20905a1ed2169ac4713f9b
Sergei Kanter <sdkanter@outlook.com>
2021-10-15 15:46:33 +0200

    ex2-3 done.

---
+++ Ex2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"in.txt";
+            int length = 10;
+            FileInfo file = new FileInfo(path);
+            if (!file.Exists)
+            {
+                file.Create();
+            }
+            using (StreamWriter swFile = new StreamWriter(path))
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < length; i++)
+                {
+                    swFile.WriteLine("{0}", rnd.Next(100));
+                }
+            }
+            int sum = 0;
+            using (StreamReader srFile = new StreamReader(path))
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    sum += Convert.ToInt32(srFile.ReadLine());
+                }
+            }
+            Console.WriteLine("Сумма чисел из файла {0} равна {1}", path, sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ex3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "in.txt";
+            using (StreamReader srFile = new StreamReader(path))
+            {
+                Console.WriteLine("Количество символов в тексте: {0}", srFile.ReadToEnd().Length);
+
+            }
+            using (StreamReader srFile = new StreamReader(path))
+            {
+
+                string[] strArray = srFile.ReadToEnd().Split('\n');
+                Console.WriteLine("Количество строк в тексте: {0}", strArray.Length);
+            }
+            using (StreamReader srFile = new StreamReader(path))
+            {
+                string[] strArray = srFile.ReadToEnd().Split();
+
+                int count = 0;
+               
+                foreach (var str in strArray)
+                {
+                    if (str != "")
+                    {
+                        count++;
+                    }
+
+                }
+                Console.WriteLine("Количество слов в тексте: {0}", count);
+            }
+            Console.ReadKey();
+        }
+    }
+}
